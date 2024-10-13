dd82a667377c059666616b3319c000100ed37bbd
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-13 18:03:54 +0300

    chekking api deploy5 ,another host

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Middleware
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "https://44ever.netlify.app/", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
