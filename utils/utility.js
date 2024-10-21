4819988d3b25b4d74b740d9950e2e102bfb6ca89
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-21 03:08:22 +0300

    handle cookies setting

--- utils/utility.js
+++ utils/utility.js
@@ -7,8 +7,8 @@ export const createJWT = (res, userID) => {
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "production",
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "None",
         maxAge: 1 * 24 * 60 * 60 * 1000, 
     });
 
