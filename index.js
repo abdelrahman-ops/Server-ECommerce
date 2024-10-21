b99997df9aacb66f86b81ff92c89fb3f3196086a
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-21 03:01:25 +0300

    add comments , packages

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
 import express from 'express';
 import "dotenv/config";
-
+import cookieParser from "cookie-parser";
 import cors from 'cors';
+import morgan from "morgan";
+
+
+import { routeNotFound, errorHandler } from './middlewares/errorMiddleware.js';
+import routes from './routes/index.js';
+import dbConnection from './config/db.js';
+
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
-import router from './routes/index.js';
-import { routeNotFound, errorHandler } from './middlewares/errorMiddleware.js';
-
-import dbConnection from './config/db.js';
-
+// Establish database connection
 dbConnection();
+
+// Create express app instance
 const app = express();
 
+// Get __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Middleware
-app.use(cors({ origin: "https://44ever.netlify.app/", credentials: true }));
+
+
+        // Middleware setup
+app.use(
+    cors({ 
+        origin: [
+            "http://localhost:5173" , 
+            "https://44ever.netlify.app"
+        ] , 
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 // Serve static files
 app.use('/images', express.static(join(__dirname, 'public/images')));
-app.use('/public/images', express.static('public/images'));
 
-app.use('/api', router);
+app.use('/users', express.static(join(__dirname, 'public/users')));
 
+app.use('/api', routes);
+
+// Handle errors
 app.use(routeNotFound);
 app.use(errorHandler);
 
-// This function will be used by Vercel to handle requests
-export default (req, res) => {
-    app(req, res);
-};
+app.get("/", (req, res) => {
+    res.send("Server is up and running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
