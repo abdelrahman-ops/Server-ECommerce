2212e935e9276ada6bd9fa5a5ba11cb674033e80
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-27 20:42:24 +0300

    handle cors

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(
     cors({ 
         origin: [
             "http://localhost:5173" , 
+            "http://localhost:5174",
             "https://44ever.netlify.app"
         ], 
         methods: ["GET", "POST", "PUT", "DELETE"],
