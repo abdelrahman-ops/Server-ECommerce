c286694d25fa873c29a956a883f1d12c53510d10
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-27 20:43:09 +0300

    update jwt args

--- utils/utility.js
+++ utils/utility.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-export const createJWT = (res, userID) => {
-    const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
+export const createJWT = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         // expiresIn: "1d",
     });
 
